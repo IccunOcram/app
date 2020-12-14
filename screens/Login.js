d33a126607e42971f7d0a4a24701b0f1ce8058be
Marco <marconucci1@gmail.com>
2020-12-14 12:52:00 +0100

    prova input

--- screens/Login.js
+++ screens/Login.js
@@ -4,23 +4,34 @@ import Row from "../components/Row.js";
 import FormButton from "../components/FormButton.js";
 import Input from "../components/Input.js";
 import { rootNavigation } from '../Utility/navigation.js';
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 
-export default function Welcome({ navigation, route }) {
+export default function Login({ navigation, route }) {
 
     const lostPassword = () => {
+        console.log('Hai cambiato la password');
+    }
+
+    const signUpForm = () => {
         navigation.navigate('SignUp')
     }
 
     return (
         <>
             <Text>Nome Utente/Email</Text>
-                <Input />
+            <Input />
             <Text>Password</Text>
-                <Input />
-                    <FormButton />
-            <Text onPress={lostPassword}>Hai dimenticato la password?</Text>
-            <Text>Hai dimenticato la password?</Text>
+            <Input />
+            <FormButton />
+            <TouchableOpacity onPress={lostPassword}>
+                <Text>Hai dimenticato la password?</Text>
+            </TouchableOpacity>
+            <Text>Non sei iscritto?
+            <TouchableOpacity onPress={signUpForm}>
+                    <Text>Registrati!</Text>
+                </TouchableOpacity>
+            </Text>
 
         </>
     )
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Text } from 'react-native'
+import Input from '../components/Input.js'
+import { View } from 'react-native'
+
+const inputs = [
+    { label: 'Username', name: 'username'}
+];
+
 
 export default function SignUp(){
     return (
-        <>
-        <Text>Ciao sono SignUp</Text>
-        </>
+        <View>
+        {inputs.map((input, index)=>
+        <Input 
+        label={input.label}
+        
+        />
+        )}
+        </View>
     )
 }
