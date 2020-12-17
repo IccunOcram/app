a5f7f7d75c8cf7ab10b01d9d8983bedc90fcf4f7
riccardozilio <riccardozilio.zilio@gmail.com>
2020-12-17 12:14:27 +0100

    pull

--- components/UpTab.js
+++ components/UpTab.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import colors from '../config/colors';
 
 export default function UpTab () {
 
     return (
         <View style={{height: '100%', backgroundColor: 'red'}}>
-            <View>
+            <View style={{height: '20%', backgroundColor: colors.blu}}>
                 <Text>logo</Text>
                 <Text>utente</Text>
             </View>
